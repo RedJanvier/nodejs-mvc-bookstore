30383eaedba4f381defdbf65921315cc2a93a6c7
RedJanvier <janvierntwali@gmail.com>
2019-11-11 11:04:53 +0200

    cleaned up the syntax in [controllers/index.js]

--- controllers/index.js
+++ controllers/index.js
@@ -1,20 +1,22 @@
 const db = require('../config/dbConfig').db;
 const usersTable = require('../config/dbConfig').tableName.users;
 
-exports.handle_dashboard_get = (req, res, next) => {
-    setTimeout(() => {
-        res.render('dashboard', { users: res.locals.users }) 
-    }, 1200);
+const get = (req, res, next) => { 
+    res.render('dashboard', { users: res.locals.users });
 };
 
-exports.addUsers = (req, res, next) => {
+const addUsers = (req, res, next) => {
     const { userId } = req.session;
     if (userId) {
-        db(usersTable).select('email', 'age', 'gender', 'name', 'id').returning('*')
-            .then(users => {
-                res.locals.users = users;
-            })
+        db(usersTable)
+            .select('email', 'age', 'gender', 'name', 'id')
+            .then(users => res.locals.users = users)
             .catch(console.log);
         }
     next();
+};
+
+module.exports = {
+    get,
+    addUsers
 };
