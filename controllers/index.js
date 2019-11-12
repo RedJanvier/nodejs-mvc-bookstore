86fb76fa36f2443e1408a3481f470ebf802b14b5
RedJanvier <janvierntwali@gmail.com>
2019-11-12 08:42:19 +0200

    [deepscan] repaired the unused bookstable

--- controllers/index.js
+++ controllers/index.js
@@ -11,58 +11,57 @@ const dashboard = (req, res, next) => {
 };
 
 const index = (req, res, next) => { 
-    // const { userId } = req.session;
-    // if (userId) {
+    const { userId } = req.session;
+    if (userId) {
 
-        // db(booksTable)
-        // .select('title', 'summary', 'img', )
-        // .then(books => res.render('welcome', { books }))
-        // .catch(console.log);
-
-        res.render('welcome', { books: [
-            { 
-                title: 'John Doe in the jungle', 
-                summary: 'Lorem ipsum dolor ipudh smen unaieaaamldn uanepaneb dna,d  naje an anaiajne ajnejnajna a;wmef', 
-                img: 'https://m.media-amazon.com/images/M/MV5BMjRjNDk2NTItZGQ1OS00OGU0LWJhMzUtNmZkNDE4NDg0MDMyXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UY1200_CR84,0,630,1200_AL_.jpg' 
-            },
-            { 
-                title: 'John Doe in the jungle', 
-                summary: 'Lorem ipsum dolor ipudh smen unaieaaamldn uanepaneb dna,d  naje an anaiajne ajnejnajna a;wmef', 
-                img: 'https://m.media-amazon.com/images/M/MV5BMjRjNDk2NTItZGQ1OS00OGU0LWJhMzUtNmZkNDE4NDg0MDMyXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UY1200_CR84,0,630,1200_AL_.jpg' 
-            },
-            { 
-                title: 'John Doe in the jungle', 
-                summary: 'Lorem ipsum dolor ipudh smen unaieaaamldn uanepaneb dna,d  naje an anaiajne ajnejnajna a;wmef', 
-                img: 'https://m.media-amazon.com/images/M/MV5BMjRjNDk2NTItZGQ1OS00OGU0LWJhMzUtNmZkNDE4NDg0MDMyXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UY1200_CR84,0,630,1200_AL_.jpg' 
-            },
-            { 
-                title: 'John Doe in the jungle', 
-                summary: 'Lorem ipsum dolor ipudh smen unaieaaamldn uanepaneb dna,d  naje an anaiajne ajnejnajna a;wmef', 
-                img: 'https://m.media-amazon.com/images/M/MV5BMjRjNDk2NTItZGQ1OS00OGU0LWJhMzUtNmZkNDE4NDg0MDMyXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UY1200_CR84,0,630,1200_AL_.jpg' 
-            },
-            { 
-                title: 'John Doe in the jungle', 
-                summary: 'Lorem ipsum dolor ipudh smen unaieaaamldn uanepaneb dna,d  naje an anaiajne ajnejnajna a;wmef', 
-                img: 'https://m.media-amazon.com/images/M/MV5BMjRjNDk2NTItZGQ1OS00OGU0LWJhMzUtNmZkNDE4NDg0MDMyXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UY1200_CR84,0,630,1200_AL_.jpg' 
-            },
-            { 
-                title: 'John Doe in the jungle', 
-                summary: 'Lorem ipsum dolor ipudh smen unaieaaamldn uanepaneb dna,d  naje an anaiajne ajnejnajna a;wmef', 
-                img: 'https://m.media-amazon.com/images/M/MV5BMjRjNDk2NTItZGQ1OS00OGU0LWJhMzUtNmZkNDE4NDg0MDMyXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UY1200_CR84,0,630,1200_AL_.jpg' 
-            },
-            { 
-                title: 'John Doe in the jungle', 
-                summary: 'Lorem ipsum dolor ipudh smen unaieaaamldn uanepaneb dna,d  naje an anaiajne ajnejnajna a;wmef', 
-                img: 'https://m.media-amazon.com/images/M/MV5BMjRjNDk2NTItZGQ1OS00OGU0LWJhMzUtNmZkNDE4NDg0MDMyXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UY1200_CR84,0,630,1200_AL_.jpg' 
-            },
-            { 
-                title: 'John Doe in the jungle', 
-                summary: 'Lorem ipsum dolor ipudh smen unaieaaamldn uanepaneb dna,d  naje an anaiajne ajnejnajna a;wmef', 
-                img: 'https://m.media-amazon.com/images/M/MV5BMjRjNDk2NTItZGQ1OS00OGU0LWJhMzUtNmZkNDE4NDg0MDMyXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UY1200_CR84,0,630,1200_AL_.jpg' 
-            }
-        ] 
-        });
-    // }
+        db(booksTable)
+        .select('title', 'summary', 'img', )
+        .then(books => res.render('welcome', { books }))
+        .catch(console.log);
+    }
+    res.render('welcome', { books: [
+        { 
+            title: 'John Doe in the jungle', 
+            summary: 'Lorem ipsum dolor ipudh smen unaieaaamldn uanepaneb dna,d  naje an anaiajne ajnejnajna a;wmef', 
+            img: 'https://m.media-amazon.com/images/M/MV5BMjRjNDk2NTItZGQ1OS00OGU0LWJhMzUtNmZkNDE4NDg0MDMyXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UY1200_CR84,0,630,1200_AL_.jpg' 
+        },
+        { 
+            title: 'John Doe in the jungle', 
+            summary: 'Lorem ipsum dolor ipudh smen unaieaaamldn uanepaneb dna,d  naje an anaiajne ajnejnajna a;wmef', 
+            img: 'https://m.media-amazon.com/images/M/MV5BMjRjNDk2NTItZGQ1OS00OGU0LWJhMzUtNmZkNDE4NDg0MDMyXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UY1200_CR84,0,630,1200_AL_.jpg' 
+        },
+        { 
+            title: 'John Doe in the jungle', 
+            summary: 'Lorem ipsum dolor ipudh smen unaieaaamldn uanepaneb dna,d  naje an anaiajne ajnejnajna a;wmef', 
+            img: 'https://m.media-amazon.com/images/M/MV5BMjRjNDk2NTItZGQ1OS00OGU0LWJhMzUtNmZkNDE4NDg0MDMyXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UY1200_CR84,0,630,1200_AL_.jpg' 
+        },
+        { 
+            title: 'John Doe in the jungle', 
+            summary: 'Lorem ipsum dolor ipudh smen unaieaaamldn uanepaneb dna,d  naje an anaiajne ajnejnajna a;wmef', 
+            img: 'https://m.media-amazon.com/images/M/MV5BMjRjNDk2NTItZGQ1OS00OGU0LWJhMzUtNmZkNDE4NDg0MDMyXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UY1200_CR84,0,630,1200_AL_.jpg' 
+        },
+        { 
+            title: 'John Doe in the jungle', 
+            summary: 'Lorem ipsum dolor ipudh smen unaieaaamldn uanepaneb dna,d  naje an anaiajne ajnejnajna a;wmef', 
+            img: 'https://m.media-amazon.com/images/M/MV5BMjRjNDk2NTItZGQ1OS00OGU0LWJhMzUtNmZkNDE4NDg0MDMyXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UY1200_CR84,0,630,1200_AL_.jpg' 
+        },
+        { 
+            title: 'John Doe in the jungle', 
+            summary: 'Lorem ipsum dolor ipudh smen unaieaaamldn uanepaneb dna,d  naje an anaiajne ajnejnajna a;wmef', 
+            img: 'https://m.media-amazon.com/images/M/MV5BMjRjNDk2NTItZGQ1OS00OGU0LWJhMzUtNmZkNDE4NDg0MDMyXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UY1200_CR84,0,630,1200_AL_.jpg' 
+        },
+        { 
+            title: 'John Doe in the jungle', 
+            summary: 'Lorem ipsum dolor ipudh smen unaieaaamldn uanepaneb dna,d  naje an anaiajne ajnejnajna a;wmef', 
+            img: 'https://m.media-amazon.com/images/M/MV5BMjRjNDk2NTItZGQ1OS00OGU0LWJhMzUtNmZkNDE4NDg0MDMyXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UY1200_CR84,0,630,1200_AL_.jpg' 
+        },
+        { 
+            title: 'John Doe in the jungle', 
+            summary: 'Lorem ipsum dolor ipudh smen unaieaaamldn uanepaneb dna,d  naje an anaiajne ajnejnajna a;wmef', 
+            img: 'https://m.media-amazon.com/images/M/MV5BMjRjNDk2NTItZGQ1OS00OGU0LWJhMzUtNmZkNDE4NDg0MDMyXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UY1200_CR84,0,630,1200_AL_.jpg' 
+        }
+    ] 
+    });
 };
 
 
