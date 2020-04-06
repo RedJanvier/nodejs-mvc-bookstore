5d29b87ea91d30f3dcd3ad4b787de4904536aeee
RedJanvier <janvierntwali@gmail.com>
2020-04-07 00:01:02 +0200

    added users seeds

--- seeds/users.js
+++ seeds/users.js
@@ -1,13 +1,15 @@
-
+`INSERT INTO borrowers(name, email, password, age, gender) VALUES ("Alain NIYONEMA", "alainyern@gmail.com", "electrotech", 21, "male"),
+("John DOE", "johndoe@gmail.com", "electrotech", 27, "male"),
+("Maliza CONSTANTINE", "malizacoco@gmail.com", "electrotech", 19, "female");`
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('borrowers').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('borrowers').insert([
+        {name: "Alain NIYONEMA", email: "alainyern@gmail.com", password: "electrotech", age: 21, gender: "male"},
+        {name: "John DOE", email: "johndoe@gmail.com", password: "electrotech", age: 27, gender: "male"},
+        {name: "Maliza CONSTANTINE", email: "malizacoco@gmail.com", password: "electrotech", age: 19, gender: "female"}
       ]);
     });
 };
