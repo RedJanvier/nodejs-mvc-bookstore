2c06d6a6dad179b739a17e24df88cd7d3a01f133
RedJanvier <janvierntwali@gmail.com>
2020-04-07 03:49:01 +0200

    added test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,13 +1,21 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+/* eslint-disable no-undef */
+import chai from 'chai';
+import chaiHttp from 'chai-http';
 
-const app = require('../app');
+import app from '../app';
 
 chai.use(chaiHttp);
 chai.should();
 
 describe('This is a simple first test', () => {
   it('should render register page', (done) => {
-    chai(app);
+    chai
+      .request(app)
+      .get('/')
+      .end((err, response) => {
+        if (err) throw err;
+        response.should.have.status(200);
+        return done();
+      });
   });
 });
